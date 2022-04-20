2b30b79ed34264534e93c7107deedd5092142b52
dron721 <omarbadr938@gmail.com>
2022-04-20 16:51:37 +0200

    more changes

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,31 +1,22 @@
-import data from "./data";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name}></img>
-              </a>
-              <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p><strong>$</strong>{product.price}</p>
-                <button>Add To Cart</button>
-              </div>
-            </div>
-          ))}
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen/>}/>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
         </div>
-      </main>
-    </div>
+    </BrowserRouter>
   );
 }
 
